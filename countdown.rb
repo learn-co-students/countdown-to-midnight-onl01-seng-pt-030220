53ce98b5428d03313884ed80decc6c9a65d898b3
Christopher Chaison <cchaison0@gmail.com>
2020-03-03 22:07:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(cd_number)
+  while cd_number > 0
+    puts "#{cd_number} SECOND(S)!"
+    cd_number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(cd_number)
+  while cd_number > 0
+    puts "#{cd_number} SECOND(S)!"
+    cd_number -=1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
