d09c36edcb13979023d052d8631747a702028d9b
YUKIKO IKEDA <applelele@gmail.com>
2020-03-06 22:06:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  
+  while number > 0
+  print "#{number} SECOND(S)!\n"
+  number -= 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number2)
+  while number2 > 0
+  print "#{number2} SECOND(S)!"
+  sleep(1)
+  number2 -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
