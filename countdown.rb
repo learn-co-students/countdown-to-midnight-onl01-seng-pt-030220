5dfe8b52077cc005c457cd65969ab58fd9ca672f
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-08 17:04:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+#write your code here 
 
-def countdown
+# INPUT --> Starting number of seconds from midnight --> Interger
+# OUTPUT --> "HAPPY NEW YEAR" --> String
+# ACTIONS --> puts out number of seconds left for each loop --> puts a string
+
+def countdown(seconds)
+while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
+
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+  puts "#{seconds} SECONDS(S)"
+  sleep(1)
+  seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
