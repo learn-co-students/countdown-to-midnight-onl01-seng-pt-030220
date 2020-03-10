e660968103fa6b5f19f0526a842ca25a73820273
Dwight Nodolf <dnodolf@gmail.com>
2020-03-10 04:40:13 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
+#input --> integers in seconds
+#output --> a string ' #SECONDS' with the seconds counted interpolated into each looped string
 
-def countdown
+    
+def countdown(seconds)
+  
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+end
+
+   return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 
+  puts "#{seconds} SECOND(S)!"
+  sleep(1) 
+  seconds -= 1 
+  
+end
+return "HAPPY NEW YEAR!" 
 end
